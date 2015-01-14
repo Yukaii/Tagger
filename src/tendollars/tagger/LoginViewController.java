a4ea66060c79d44cf4b57ae9f98b77ba46bb223c
David <yoxisem544@gmail.com>
2015-01-15 00:09:37 +0800

    Login

---
+++ src/tendollars/tagger/LoginViewController.java
@@ -0,0 +1,127 @@
+package tendollars.tagger;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Modality;
+import javafx.stage.PopupBuilder;
+import javafx.stage.Stage;
+import org.controlsfx.dialog.DialogStyle;
+import org.controlsfx.dialog.Dialogs;
+import tendollars.tagger.model.FileInfo;
+import tendollars.tagger.utils.TagUtil;
+
+
+import javax.swing.*;
+import javax.swing.plaf.nimbus.State;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Optional;
+
+
+/**
+ * Created by David on 1/14/15.
+ */
+public class LoginViewController {
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button loginButton;
+
+    private MainApp mainApp;
+    private Stage primaryStage;
+    private State stage;
+
+    public boolean islog = false;
+
+    public LoginViewController() {
+
+    }
+    @FXML
+    private void initialize() {
+        loginButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if (event.getClickCount() != 0)
+                    if (username.getText().toString().equals("hello") && password.getText().equals("ya"))
+                    {
+                        islog = true;
+                        Stage s = (Stage) loginButton.getScene().getWindow();
+                        s.close();
+                        primaryStage.show();
+                    } else {
+//                        showStage();
+                        alert();
+                    }
+            }
+        });
+    }
+
+    public static void showStage(){
+        Stage newStage = new Stage();
+        VBox comp = new VBox();
+        Label alert = new Label("Name");
+        comp.getChildren().add(alert);
+
+        Scene stageScene = new Scene(comp, 300, 300);
+        newStage.setScene(stageScene);
+        newStage.initModality(Modality.WINDOW_MODAL);
+
+        newStage.show();
+    }
+
+
+
+    public static void alert() {
+        try {
+            Stage newStage = new Stage();
+            FXMLLoader l = new FXMLLoader();
+            l.setLocation(MainApp.class.getResource("view/warn.fxml"));
+
+            Pane warn = (Pane) l.load();
+
+//            WarnViewController warnViewController = l.getController();
+
+
+            Scene stageScene = new Scene(warn);
+            newStage.setScene(stageScene);
+            newStage.requestFocus();
+
+            newStage.initModality(Modality.WINDOW_MODAL);
+            newStage.setResizable(false);
+
+            newStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setMainApp(MainApp mainapp) {
+        this.mainApp = mainapp;
+    }
+
+    public void setPrimaryStage(Stage stage) {
+        this.primaryStage = stage;
+    }
+
+
+}
--- src/tendollars/tagger/MainApp.java
+++ src/tendollars/tagger/MainApp.java
@@ -4,6 +4,7 @@ import com.sun.deploy.util.FXLoader;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
@@ -12,12 +13,17 @@ import javafx.stage.Stage;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.DirectoryFileFilter;
 import org.apache.commons.io.filefilter.RegexFileFilter;
+import sun.rmi.runtime.Log;
 import tendollars.tagger.model.FileInfo;
 import tendollars.tagger.utils.TagUtil;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowEvent;
+import java.beans.EventHandler;
 import java.io.IOException;
 import java.io.File;
 import java.util.Collection;
+import java.util.EventListener;
 
 /**
  * Created by yukai on 2015/1/14.
@@ -29,17 +35,33 @@ public class MainApp extends Application {
     private ObservableList<FileInfo> fileInfos = FXCollections.observableArrayList();
     private File lastAccess;
 
+    LoginViewController loginViewController;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         this.primarySage = primaryStage;
         this.primarySage.setTitle("Tagger");
 
-        this.lastAccess = new File("/Users/");
+        showLoginView();
+//        ashowLoginView();
+        this.lastAccess = new File("/Users/David/Desktop/");
         for (File file : TagUtil.scanFile(this.lastAccess)) {
             fileInfos.add(new FileInfo(file));
         }
+
         showFileOverview();
+        this.primarySage.hide();
+
+        this.primarySage.setOnCloseRequest(new javafx.event.EventHandler<javafx.stage.WindowEvent>() {
+            @Override
+            public void handle(javafx.stage.WindowEvent event) {
+                primaryStage.hide();
+                showLoginView();
+            }
+        });
+
+
+
     }
 
 
@@ -56,10 +78,36 @@ public class MainApp extends Application {
 
             Scene scene = new Scene(fileOverview);
             primarySage.setScene(scene);
+            primarySage.setResizable(false);
             primarySage.show();
 
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+
+    public void showLoginView() {
+        try {
+            Stage sa = new Stage();
+            sa.setTitle("Login");
+            FXMLLoader l = new FXMLLoader();
+            l.setLocation(MainApp.class.getResource("view/login.fxml"));
+            AnchorPane login = (AnchorPane) l.load();
+
+            // add controller
+            this.loginViewController = l.getController();
+            this.loginViewController.setMainApp(this);
+            this.loginViewController.setPrimaryStage(this.primarySage);
+            //
+
+            Scene s = new Scene(login);
+            sa.setResizable(false);
+            sa.setScene(s);
+            sa.show();
+
+        } catch (IOException ee) {
+            ee.printStackTrace();
         }
     }
 
