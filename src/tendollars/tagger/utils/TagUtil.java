c9a327521385e830d42d122fdd6e30ca7e478b56
Yukai Huang <yukaihuang1993@hotmail.com>
2015-01-15 23:21:26 +0800

    refract openDirectory into TagUtlis

--- src/tendollars/tagger/utils/TagUtil.java
+++ src/tendollars/tagger/utils/TagUtil.java
@@ -1,12 +1,17 @@
 package tendollars.tagger.utils;
 
 import javafx.collections.ObservableList;
+import javafx.scene.control.Alert;
+import javafx.stage.DirectoryChooser;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.DirectoryFileFilter;
 import org.apache.commons.io.filefilter.RegexFileFilter;
+import tendollars.tagger.MainApp;
+import tendollars.tagger.db.DaoManager;
 import tendollars.tagger.model.FileInfo;
 
 import java.io.File;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -55,6 +60,47 @@ public class TagUtil {
         arr.clear();
         arr.addAll(hs);
         return arr;
+    }
+    public static ObservableList<FileInfo> openDirectory(MainApp mainApp) throws SQLException {
+        return openDirectory(mainApp, false);
+    }
+
+    public static ObservableList<FileInfo> openDirectory(MainApp mainApp, boolean chooseDefault) throws SQLException {
+        if (!chooseDefault) {
+            DirectoryChooser chooser = new DirectoryChooser();
+            chooser.setTitle("Choose Start Directory");
+            File defaultDirectory = mainApp.getLastAccess();
+            chooser.setInitialDirectory(defaultDirectory);
+
+            File chooseDir = null;
+            while (chooseDir == null) {
+                chooseDir = chooser.showDialog(mainApp.getPrimarySage());
+                if (chooseDir == null) {
+                    Alert alert = new Alert(Alert.AlertType.WARNING);
+                    alert.setTitle("Choose Directory Error");
+                    alert.setHeaderText(null);
+                    alert.setContentText("You Muse Choose a Directory!");
+                    alert.showAndWait();
+                }
+            }
+            mainApp.setLastAccess(chooseDir);
+        }
+
+        Collection<File> files = TagUtil.scanFile(mainApp.getLastAccess());
+        ArrayList<FileInfo> filelist = new ArrayList<FileInfo>();
+
+        for (File file : files) {
+            filelist.add(new FileInfo(file));
+        }
+        return DaoManager.loadFiles(filelist);
+    }
+
+    public static File openDir(MainApp mainApp) {
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("Choose Start Directory");
+        File defaultDirectory = mainApp.getLastAccess();
+        chooser.setInitialDirectory(defaultDirectory);
+        return chooser.showDialog(mainApp.getPrimarySage());
     }
 
 }
